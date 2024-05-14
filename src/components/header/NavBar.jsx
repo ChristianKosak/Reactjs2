6641d20e5aba341a832fc4017ab1c3f996acc0a5
Christian Kosak <christiankosak@hotmail.com>
2024-05-15 01:01:00 +0200

    Actualizacion

--- src/components/header/NavBar.jsx
+++ src/components/header/NavBar.jsx
@@ -6,9 +6,9 @@ import Carrito from "./Carrito"
               <nav className="nav">
             <ul>
             <li className="nav-item"><a className="nav-link" href="#">Inicio</a></li>
-            <li className="nav-item"><a className="nav-link" href="#">Camisetas</a></li>
-            <li className="nav-item"><a className="nav-link" href="#">Pantalones</a></li>
-            <li className="nav-item"><a className="nav-link" href="#">Camperas</a></li>
+            <li className="nav-item"><a className="nav-link" href="#">Smartphones</a></li>
+            <li className="nav-item"><a className="nav-link" href="#">Notebooks</a></li>
+            <li className="nav-item"><a className="nav-link" href="#">Consolas</a></li>
             </ul>
         </nav>
 
